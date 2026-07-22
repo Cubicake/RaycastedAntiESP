a12168d50289919833e70a07ecc265524dcabef3
Cubicake <70577278+Cubicake@users.noreply.github.com>
2026-07-22 22:11:29 +0930

    Bypass hide on spawn for excluded entities

--- platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/packets/PaperPacketEventsEntityViewController.java
+++ platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/packets/PaperPacketEventsEntityViewController.java
@@ -10,15 +10,50 @@ package games.cubi.raycastedantiesp.paper.packets;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import com.github.retrooper.packetevents.wrapper.PacketWrapper;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSpawnEntity;
+import games.cubi.logs.Logger;
+import games.cubi.raycastedantiesp.core.config.raycast.EntityTypeExclusions;
+import games.cubi.raycastedantiesp.core.players.PlayerData;
+import games.cubi.raycastedantiesp.core.tracked.NettyEntity;
 import games.cubi.raycastedantiesp.packetevents.viewcontrollers.PacketEventsEntityViewController;
 
+import java.util.UUID;
 import java.util.function.IntSupplier;
-
 
 public class PaperPacketEventsEntityViewController extends PacketEventsEntityViewController {
 
     public PaperPacketEventsEntityViewController(IntSupplier currentTickSupplier) {
         super(currentTickSupplier);
         PacketEvents.getAPI().getEventManager().registerListener(this, PacketListenerPriority.HIGHEST);
+    }
+
+    @Override
+    protected boolean handleEntitySpawn(PacketWrapper<?> packet, int entityID, boolean isPlayer,
+                                        PlayerData playerData, UUID world, int currentTick) {
+        if (isPlayer || world == null) {
+            return super.handleEntitySpawn(packet, entityID, isPlayer, playerData, world, currentTick);
+        }
+
+        WrapperPlayServerSpawnEntity spawnPacket = (WrapperPlayServerSpawnEntity) packet;
+        int entityType = spawnPacket.getEntityType().getId(
+                PacketEvents.getAPI().getServerManager().getVersion().toClientVersion()
+        );
+        if (!EntityTypeExclusions.excludes(entityType)) {
+            return super.handleEntitySpawn(packet, entityID, false, playerData, world, currentTick);
+        }
+
+        NettyEntity<?> entity = Logger.requireNonNull(
+                processEntitySpawn(playerData, packet, world, currentTick),
+                "processEntitySpawn returned null",
+                3,
+                PaperPacketEventsEntityViewController.class
+        );
+        entity.setVisible(true);
+        entity.setClientVisible(true);
+        entity.setLastChecked(currentTick);
+        insertEntityToEntityView(entity, playerData, world);
+        playerData.nettyData().runPendingPostSpawnTaskForEntity(entityID);
+        return false;
     }
 }
