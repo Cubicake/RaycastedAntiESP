93949b79060cac87e34ee3da1aacca020c1cc8c5
Cubicake <70577278+Cubicake@users.noreply.github.com>
2026-07-30 21:18:54 +0930

    Bump version

--- platform-paper/build.gradle.kts
+++ platform-paper/build.gradle.kts
@@ -64,7 +64,7 @@ val javaToolchainService = project.extensions.getByType(JavaToolchainService::cl
 
 group = "games.cubi.raycastedantiesp.paper"
 
-val platformPaperVersion: String = "0.9.0-SNAPSHOT"
+val platformPaperVersion: String = "0.10.0-SNAPSHOT"
 val coreVersion = project(":core").version.toString()
 
 val commitShort = providers.exec {
