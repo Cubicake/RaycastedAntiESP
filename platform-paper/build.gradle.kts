9a822af8bc8eb620a6ebc5decabaf4167ce27268
Cubicake <70577278+Cubicake@users.noreply.github.com>
2026-08-19 15:08:01 +0930

    Remove FancyCompatibility.java (moving to bridge)

--- platform-paper/build.gradle.kts
+++ platform-paper/build.gradle.kts
@@ -21,7 +21,6 @@ repositories {
     maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
     maven { url = uri("https://repo.codemc.io/repository/maven-snapshots/") }
     maven { url = uri("https://eldonexus.de/repository/maven-public/") }
-    maven("https://repo.fancyinnovations.com/releases")
 }
 
 dependencies {
@@ -30,9 +29,6 @@ dependencies {
     compileOnly("com.github.retrooper:packetevents-spigot:2.12.0")
     compileOnly("org.spongepowered:configurate-core:4.2.0")
     compileOnly("org.spongepowered:configurate-yaml:4.2.0")
-
-    compileOnly("de.oliver:FancyHolograms:2.9.1")
-    compileOnly("de.oliver:FancyNpcs:2.9.2")
 
     compileOnly("net.strokkur.commands:annotations-paper:2.1.2")
     annotationProcessor("net.strokkur.commands:processor-paper:2.1.2")
